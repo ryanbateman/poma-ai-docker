50eda8fbeef525b83dbb54f5a4362719b6a1785e
Ryan Bateman <secretdark@gmail.com>
2026-01-12 12:11:22 +0100

    Implement bi-directional Chunkset integration in Weaviate

---
+++ inspect_sdk.py
@@ -0,0 +1,10 @@
+
+import poma
+from poma import Poma
+import inspect
+
+print("DOCSTRING for start_chunk_file:")
+print(Poma.start_chunk_file.__doc__)
+
+print("\nDOCSTRING for get_chunk_result:")
+print(Poma.get_chunk_result.__doc__)
